0c4f9fcbc71f3f68fb909262190a357f6dda464f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-06-25 00:26:56 +0000

    FEA: setup.py update

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
 
-setup(name='hardDecisions',
-      version='0.0.2',
-      description='Decision making tree modeling using Python',
-      long_description='Decision making tree modeling using Python',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Office/Business :: Financial',
-      ],
-      keywords='decision-tree decision-making operations-research analytics',
-      url='http://github.com/jdvelasq/hardDecisions',
-      author='Juan D. Velasquez & Ibeth K. Vergara',
-      author_email='jdvelasq@unal.edu.co',
-      license='MIT',
-      packages=['hardDecisions'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name="decisions_tree",
+    version="0.1.0",
+    author="Juan D. Velasquez",
+    author_email="jdvelasq@unal.edu.co",
+    license="MIT",
+    url="http://github.com/jdvelasq/decisions-tree",
+    description="Decision Analysis in Python",
+    long_description="Decisoion Tree Analysis in Python",
+    keywords="analytics",
+    platforms="any",
+    provides=["decisions_tree"],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+    packages=find_packages(),
+    package_dir={"decisions_tree": "decisions_tree"},
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
